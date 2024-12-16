6cff886c55cdc5594be2975cd62a7deb4bb69e70
myxi <myxi@envs.net>
2024-12-16 13:54:10 +0530

    feat: impl (bare-bones) repl

--- src/ergo/__init__.py
+++ src/ergo/__init__.py
@@ -14,15 +14,32 @@ def process_file(file: typing.TextIO) -> None:
     util._pprint(ast)
 
 
+def repl():
+    while True:
+        try:
+            text = util.CustomStringIO(input(">>> "))
+        except EOFError:
+            print()
+            exit()
+        except KeyboardInterrupt:
+            print()
+            continue
+        tokens = lexer.make_tokens(text)
+        ast = parser.make_ast(tokens)
+        util._pprint(ast)
+
+
 def main() -> None:
     logging.basicConfig(level="DEBUG")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", type=argparse.FileType("r"))
+    parser.add_argument("file", type=argparse.FileType("r"), nargs="?")
     args = parser.parse_args()
 
     if args.file:
         process_file(args.file)
+    else:
+        repl()
 
 
 if __name__ == "__init__":
